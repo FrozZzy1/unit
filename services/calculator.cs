4da2e0725eca97ac941669d742e5d6acbf970e14
FrozZzy1 <frozzzy71@gmail.com>
2024-03-16 03:29:11 +0700

    initial commit

---
+++ services/calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1.services
+{
+    class CalculatorService
+    {
+        public static List<double> Calculate(double k, double N, int countIterations)
+        {
+            List<double> results = new List<double>();
+            for (double x = 0; x <= k; x += 0.1)
+            {
+                countIterations--;
+                if (countIterations <= 0)
+                {
+                    return results;
+                }
+
+                try
+                {
+                    results.Add(1 / (x - N));
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine($"failed divisions by zero");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"failed going out of range");
+                }
+            }
+            return results;
+        }
+    }
+}
